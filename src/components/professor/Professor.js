f6bd23cacea0e67ad608f1161eb1ab9da52cb88e
wil.i.am <wiliam.tae@gmail.com>
2019-11-09 13:28:49 -0300

    style error correction

--- src/components/professor/Professor.js
+++ src/components/professor/Professor.js
@@ -2,7 +2,7 @@ import React from 'react'
 import style from './Professor.module.css'
 
 const Professor = () => (
-  <div className={style.container}>
+  <section className={style.container}>
     <div className={style.professorDiv}>
     <p className={style.title}>Quem é o professor?</p>
     <span className={style.span}></span>
@@ -13,7 +13,7 @@ const Professor = () => (
     <p className={style.text}>Podemos já vislumbrar o modo pelo qual o acompanhamento das preferências de consumo talvez venha a ressaltar a relatividade dos relacionamentos verticais entre as hierarquias.Podemos já vislumbrar o modo pelo qual o acompanhamento das preferências de consumo talvez venha a ressaltar a relatividade dos relacionamentos verticais entre as hierarquias.</p>
 
   </div>
-  </div>
+  </section>
 )
 
 export default Professor
