1e73e7250793eb04077b8e8dab138e6dcb1f41db
wil.i.am <wiliam.tae@gmail.com>
2019-11-09 16:13:22 -0300

    cards style and props added

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -10,14 +10,51 @@ const Calendar = () => (
       number='1'
       date='01/01/01'
       title='title'
-      text='text'
+      text='vislumbrar o modo pelo qual o acompanhamento das preferências de consumo talvez venha a ressaltar a relatividade.'
     />
     <Cards
       number='2'
       date='01/01/01'
       title='title'
-      text='text'
+      text='vislumbrar o modo pelo qual o acompanhamento das preferências de consumo talvez venha a ressaltar a relatividade.'
     />
+    <Cards
+      number='3'
+      date='01/01/01'
+      title='title'
+      text='vislumbrar o modo pelo qual o acompanhamento das preferências de consumo talvez venha a ressaltar a relatividade.'
+    />
+    <Cards
+      number='4'
+      date='01/01/01'
+      title='title'
+      text='vislumbrar o modo pelo qual o acompanhamento das preferências de consumo talvez venha a ressaltar a relatividade.'
+    />
+    <Cards
+      number='5'
+      date='01/01/01'
+      title='title'
+      text='vislumbrar o modo pelo qual o acompanhamento das preferências de consumo talvez venha a ressaltar a relatividade.'
+    />
+    <Cards
+      number='6'
+      date='01/01/01'
+      title='title'
+      text='vislumbrar o modo pelo qual o acompanhamento das preferências de consumo talvez venha a ressaltar a relatividade.'
+    />
+    <Cards
+      number='7'
+      date='01/01/01'
+      title='title'
+      text='vislumbrar o modo pelo qual o acompanhamento das preferências de consumo talvez venha a ressaltar a relatividade.'
+    />
+    <Cards
+      number='8'
+      date='01/01/01'
+      title='title'
+      text='vislumbrar o modo pelo qual o acompanhamento das preferências de consumo talvez venha a ressaltar a relatividade.'
+    />
+
 
     </div>
   </section>
