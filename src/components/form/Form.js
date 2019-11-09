6501eba2e00b32745753e83273707c3df36d2d5e
wil.i.am <wiliam@wiliams-MacBook-Pro.local>
2019-11-08 21:23:28 -0300

    form styling input

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -5,13 +5,13 @@ const Form = () => (
 
   <form className={style.form}>
     <p className={style.name}>Primeiro nome</p>
-    <input className={style.nameInput}/>
+    <input className={style.nameInput} type="text" placeholder="Primeiro Nome"/>
 
     <p className={style.email}>Email</p>
-    <input className={style.emailInput}/>
+    <input className={style.emailInput} type="text" placeholder="Email"/>
 
     <p className={style.whatsapp}>Whatsapp</p>
-    <input className={style.whatsappInput}/>
+    <input className={style.whatsappInput} type="number" placeholder="Whatsapp"/>
 
     <button className={style.formButton}>Quero assistir!</button>
   </form>
