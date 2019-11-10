be517cfbfb07aa5759f3ffa1e172d59aff440ca0
wil.i.am <wiliam.tae@gmail.com>
2019-11-10 13:43:19 -0300

    form hook added

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,21 +1,31 @@
 import React from 'react'
 import style from './Form.module.css'
+import {useState} from 'react'
 
-const Form = () => (
 
-  <form className={style.form}>
-    <p className={style.name}>Primeiro nome</p>
-    <input className={style.nameInput} type="text" placeholder="Primeiro Nome"/>
+const Form = () => {
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+  const [phone, setPhone] = useState()
 
-    <p className={style.email}>Email</p>
-    <input className={style.emailInput} type="text" placeholder="Email"/>
-
-    <p className={style.whatsapp}>Whatsapp</p>
-    <input className={style.whatsappInput} type="number" placeholder="Whatsapp"/>
-
-    <button className={style.formButton}>Quero assistir!</button>
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log('success', name, email, phone)
+  }
+  return (
+  <form className={style.form} onSubmit={handleSubmit}>
+    <label className={style.name}>Primeiro nome
+      <input className={style.nameInput} type="text" placeholder="Primeiro Nome" required onChange={e => setName(e.target.value)}/>
+    </label>
+    <label className={style.email}>Email
+      <input className={style.emailInput} type="text" placeholder="Email" required onChange={e => setEmail(e.target.value)}/>
+    </label>
+    <label className={style.whatsapp}>Whatsapp
+      <input className={style.whatsappInput} type="number" placeholder="Whatsapp" required onChange={e => setPhone(e.target.value)}/>
+    </label>
+    <button className={style.formButton} type="submit" >Quero assistir!</button>
   </form>
 
 )
-
+}
 export default Form;
